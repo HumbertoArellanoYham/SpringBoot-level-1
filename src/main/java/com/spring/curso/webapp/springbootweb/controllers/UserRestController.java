abfeabc3a490003de78511fdeb1cea29da04b952
HumbertoArellano <humbertoyham25@gmail.com>
2024-02-20 09:06:39 -0700

    Api Rest

--- src/main/java/com/spring/curso/webapp/springbootweb/controllers/UserRestController.java
+++ src/main/java/com/spring/curso/webapp/springbootweb/controllers/UserRestController.java
@@ -1,15 +1,24 @@
 package com.spring.curso.webapp.springbootweb.controllers;
 
-import org.springframework.stereotype.Controller;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-
-@Controller
+@RestController
+@RequestMapping("/api")
 public class UserRestController {
 
-    @GetMapping("")
-    public String getMethodName(@RequestParam String param) {
-        return new String();
+    @GetMapping("/details")
+    public Map<String, Object> getMethodName() {
+        Map<String, Object> body = new HashMap<>();
+        body.put("title", "Hello world en spring boot!");
+        body.put("name", "Humberto");
+        body.put("lastname", "Arellano");
+
+        return body;
     }
 }
